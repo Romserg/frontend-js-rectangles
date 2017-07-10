84e83ec5de0578b77967656fe99b9ae0f35b4dbb
Romserg <serg.romankov@gmail.com>
2017-07-10 18:54:07 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-﻿
+﻿function areIntersected(elem1, elem2) {
+	return elem1.left < elem2.height && elem1.height > elem2.left && elem1.top < elem2.height && elem1.height > elem2.top;
+}
+
+
+
+areIntersected({
+	left: 0,
+	top: 0,
+	width: 20,
+	height: 20
+}, {
+		left: 10,
+		top: 10,
+		width: 25,
+		height: 25
+	});
